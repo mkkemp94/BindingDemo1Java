2c4d604bb6a2264bbdfdb148fd03b03c4770dac3
MKemp <mkemp@accu-time.com>
2020-12-31 12:20:17 -0500

    Fill out starter project to show hard coded student name and email

--- app/src/main/java/com/mkemp/bindingdemo1java/MainActivity.java
+++ app/src/main/java/com/mkemp/bindingdemo1java/MainActivity.java
@@ -1,15 +1,33 @@
 package com.mkemp.bindingdemo1java;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity
 {
+    private TextView nameText;
+    private TextView emailText;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        nameText = findViewById(R.id.tvStudentName);
+        emailText = findViewById(R.id.tvStudentEmail);
+        
+        nameText.setText(getCurrentStudent().getStudentName());
+        emailText.setText(getCurrentStudent().getStudentEmail());
+    }
+    
+    private Student getCurrentStudent()
+    {
+        Student student = new Student();
+        student.setStudentName("Koda");
+        student.setStudentEmail("KodaK@gmail.com");
+        return student;
     }
 }
